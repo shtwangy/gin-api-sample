e701858ec578139bcdea684d15f4c9d7fff24321
shtwangy <shtwangy@gmail.com>
2020-11-09 01:08:01 +0900

    feat(server.go): create sample api.

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type User struct {
+	Name  string `form:"name"`
+	Email string `form:"email"`
+}
+
+func main() {
+	r := gin.Default()
+	r.GET("/user", show)
+	r.POST("/users", display)
+
+	r.Run(":1323")
+}
+
+func show(c *gin.Context) {
+	u := User{}
+	err := c.Bind(&u)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Bad request")
+		return
+	}
+	c.JSON(http.StatusOK, u)
+}
+
+func display(c *gin.Context) {
+	u := User{}
+	err := c.Bind(&u)
+	if err != nil {
+		c.String(http.StatusBadRequest, "Bad request")
+		return
+	}
+	c.JSON(http.StatusOK, u)
+}
